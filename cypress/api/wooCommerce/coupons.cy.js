10bfcdf04c08df5ea068e15fe2a4be808d43f096
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-11-11 16:32:23 -0300

    create coupons; faker added

--- cypress/api/wooCommerce/coupons.cy.js
+++ cypress/api/wooCommerce/coupons.cy.js
@@ -1,10 +1,29 @@
+import { faker } from "@faker-js/faker";
+
 import tokenFixture from "../../fixtures/token.json";
+import couponsFixture from "../../fixtures/coupons.json";
 
 describe("Coupons", () => {
   it("Listar todos os coupons", () => {
     cy.getCouponsWooCommerce(tokenFixture.token).then((response) => {
       expect(response).to.exist;
       expect(response.status).to.eq(200);
+    });
+  });
+
+  it("Criar Coupon", () => {
+    let code = faker.datatype.uuid();
+    cy.postCouponsWooCommerce(
+      tokenFixture.token,
+      code,
+      couponsFixture.validCoupon.discount_type,
+      couponsFixture.validCoupon.amount,
+      couponsFixture.validCoupon.individual_use,
+      couponsFixture.validCoupon.exclude_sale_items,
+      couponsFixture.validCoupon.minimum_amount
+    ).then((response) => {
+      expect(response).to.exist;
+      expect(response.status).to.eq(201);
     });
   });
 });
--- cypress/support/requests/wooCommerce/coupons.js
+++ cypress/support/requests/wooCommerce/coupons.js
@@ -7,3 +7,33 @@ Cypress.Commands.add("getCouponsWooCommerce", (token) => {
     },
   });
 });
+
+Cypress.commands.add(
+  "postCouponsWooCommerce",
+  (
+    token,
+    code,
+    discount_type,
+    amount,
+    individual_use,
+    exclude_sale_items,
+    minimum_amount
+  ) => {
+    cy.request({
+      method: "POST",
+      url: Cypress.env("wooCommerce") + Cypress.env("coupons"),
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+      body: {
+        code: code,
+        discount_type: discount_type,
+        amount: amount,
+        individual_use: individual_use,
+        exclude_sale_items: exclude_sale_items,
+        minimum_amount: minimum_amount,
+      },
+    });
+  }
+);
