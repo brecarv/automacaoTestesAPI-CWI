947b7ba3f8d7c2de64a0ab8a09b6d8bf52b25175
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-11-11 17:04:55 -0300

    create coupons - validation

--- cypress/api/wooCommerce/coupons.cy.js
+++ cypress/api/wooCommerce/coupons.cy.js
@@ -2,12 +2,13 @@ import { faker } from "@faker-js/faker";
 
 import tokenFixture from "../../fixtures/token.json";
 import couponsFixture from "../../fixtures/coupons.json";
+import statusFixture from "../../fixtures/status.json";
 
 describe("Coupons", () => {
   it("Listar todos os coupons", () => {
     cy.getCouponsWooCommerce(tokenFixture.token).then((response) => {
       expect(response).to.exist;
-      expect(response.status).to.eq(200);
+      expect(response.status).to.eq(statusFixture.ok);
     });
   });
 
@@ -22,8 +23,21 @@ describe("Coupons", () => {
       couponsFixture.validCoupon.exclude_sale_items,
       couponsFixture.validCoupon.minimum_amount
     ).then((response) => {
-      expect(response).to.exist;
-      expect(response.status).to.eq(201);
+      expect(response.status).to.eq(statusFixture.created);
+      expect(response.body.code).to.eq(code);
+      expect(response.body.discount_type).to.eq(
+        couponsFixture.validCoupon.discount_type
+      );
+      expect(response.body.amount).to.eq(couponsFixture.validCoupon.amount);
+      expect(response.body.individual_use).to.eq(
+        couponsFixture.validCoupon.individual_use
+      );
+      expect(response.body.exclude_sale_items).to.eq(
+        couponsFixture.validCoupon.exclude_sale_items
+      );
+      expect(response.body.minimum_amount).to.eq(
+        couponsFixture.validCoupon.minimum_amount
+      );
     });
   });
 });
